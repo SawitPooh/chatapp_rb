911c55210835130750a64977f804dab8d95a9f30
Sawit Pooh <sawit.j@kkumail.com>
2025-05-05 15:27:02 +0700

    fix grammars and add context

--- spec/system/user_can_edit_room_name_spec.rb
+++ spec/system/user_can_edit_room_name_spec.rb
@@ -1,38 +1,40 @@
 require 'rails_helper'
 
 describe "edit process", type: :feature do
+  context "when a room exists, the user can edit the room name" do
   before :each do
     @room = Room.create!(name: 'test room')
   end
 
   it "edits room name" do
-    user_go_room_page
-    user_go_edit_room_name
-    user_fill_edit_room_name
-    user_submit_edit_room_name
+    user_goes_room_page
+    user_goes_edit_room_name
+    user_fills_edit_room_name
+    user_submits_edit_room_name
     user_should_see_updated_room_name
   end
 
-  def user_go_room_page
+  def user_goes_room_page
     visit rooms_path
   end
 
-  def user_go_edit_room_name
+  def user_goes_edit_room_name
     visit edit_room_path(@room)
   end
 
-  def user_fill_edit_room_name
+  def user_fills_edit_room_name
     within '#actions' do
       fill_in 'room_name', with: 'edit room'
     end
   end
 
-  def user_submit_edit_room_name
+  def user_submits_edit_room_name
     click_button 'Update Room'
   end
 
   def user_should_see_updated_room_name
     expect(page).to have_content('edit room')
   end
-  
+
+  end
 end
